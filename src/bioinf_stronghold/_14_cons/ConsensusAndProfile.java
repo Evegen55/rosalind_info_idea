1990fd608355a7357d700b2e8cb6e8fbee65b015
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-08-18 23:47:41 +0300

    TODO!!!

--- src/bioinf_stronghold/_14_cons/ConsensusAndProfile.java
+++ src/bioinf_stronghold/_14_cons/ConsensusAndProfile.java
@@ -1,6 +1,5 @@
 package bioinf_stronghold._14_cons;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +28,50 @@ public class ConsensusAndProfile {
             }
         }
 
+        printMapOnlyValues(profileMap);
+        findConcensus(profileMap, profileRowLength);
+
+        throw new UnsupportedOperationException("\n TODO");
+    }
+
+    private void findConcensus(Map<Character, int[]> profileMap, int profileRowLength) {
+        int a = 0;
+        int c = 0;
+        int g = 0;
+        int t = 0;
+        int maximum = 0;
+        int[] arr_max;
+
+        System.out.println();
+        System.out.println();
+        for (int i = 0; i < profileRowLength; i++) {
+            arr_max = new int[4];
+            int indexOfMax = 0;
+            a = profileMap.get('A')[i];
+            c = profileMap.get('C')[i];
+            g = profileMap.get('G')[i];
+            t = profileMap.get('T')[i];
+            arr_max[0] = a;
+            arr_max[1] = c;
+            arr_max[2] = g;
+            arr_max[3] = t;
+
+            //find max value
+            for (int q = 0; q < 4; q++) {
+                if (arr_max[q] > maximum) {
+                    maximum = arr_max[q];
+                }
+            }
+
+            System.out.print(a + " " + c + " " + g + " " + t + "\t" + "maximum " + maximum + "\n");
+            maximum = 0;
+
+
+        }
+        //System.out.println();
+    }
+
+    private void printMapOnlyValues(Map<Character, int[]> profileMap) {
         for (int i : profileMap.get('A')) {
             System.out.print(i + " ");
         }
@@ -44,7 +87,5 @@ public class ConsensusAndProfile {
         for (int i : profileMap.get('T')) {
             System.out.print(i + " ");
         }
-
-        throw new UnsupportedOperationException("\n TODO");
     }
 }
