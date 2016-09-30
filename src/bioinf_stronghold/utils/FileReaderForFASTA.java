e84d0a4816ea0baf5882d03a54a2a33cb756fc31
Evgenii_Lartcev <demerbrise@gmail.com>
2016-09-30 15:46:23 +0300

    GPL v_3.0

--- src/bioinf_stronghold/utils/FileReaderForFASTA.java
+++ src/bioinf_stronghold/utils/FileReaderForFASTA.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Lartsev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package bioinf_stronghold.utils;
 
 import java.io.BufferedReader;
@@ -18,11 +34,11 @@ public class FileReaderForFASTA {
      * @see <a href="https://ru.wikipedia.org/wiki/FASTA">WIKIPEDIA</a>
      */
     public static HashMap<String, String> getMapFromFile(String filename) {
-        HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<String, String> map = new HashMap<>();
         StringBuffer sb = new StringBuffer();
         File f = new File(filename);
         String label = "";
-        try (BufferedReader br = new BufferedReader(new FileReader(f));) {
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
             while (br.ready()) {
                 String line = br.readLine();
                 if (line.startsWith(">")) {
