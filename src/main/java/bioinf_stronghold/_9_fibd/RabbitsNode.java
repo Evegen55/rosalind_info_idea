c0bf07ac71bff11ea935f1cf90db8c20d9ebd367
Evgenii_Lartcev <demerbrise@gmail.com>
2018-08-01 00:46:52 +0300

    historical tree of rabbits

---
+++ src/main/java/bioinf_stronghold/_9_fibd/RabbitsNode.java
@@ -0,0 +1,114 @@
+package bioinf_stronghold._9_fibd;
+
+import java.util.LinkedList;
+
+public class RabbitsNode {
+
+    private boolean isPairAlive = true;
+
+    private long pairAbsoluteCounter; //in month
+
+    private long pairLiveCounter; //in month
+
+    private RabbitsNode lowerPairAsChild;
+
+    private RabbitsNode upperPairAsSelfLink;
+
+    public RabbitsNode() {
+    }
+
+    public RabbitsNode(long pairLiveCounter) {
+        pairAbsoluteCounter = pairLiveCounter;
+        this.pairLiveCounter = pairLiveCounter;
+    }
+
+
+    public RabbitsNode getLowerPairAsChild() {
+        return lowerPairAsChild;
+    }
+
+    public RabbitsNode getUpperPairAsSelfLink() {
+        return upperPairAsSelfLink;
+    }
+
+    public void setPairAbsoluteCounter(long pairAbsoluteCounter) {
+        this.pairAbsoluteCounter = pairAbsoluteCounter;
+    }
+
+    public void setPairLiveCounter(long pairLiveCounter) {
+        this.pairLiveCounter = pairLiveCounter;
+    }
+
+    public static void addHistoricalPairNode(RabbitsNode rabbitsNode) {
+        if (rabbitsNode != null) {
+            if (rabbitsNode.isPairAlive) {
+                long tempCounter = rabbitsNode.pairLiveCounter - 1;
+                if (rabbitsNode.pairLiveCounter == rabbitsNode.pairAbsoluteCounter) { //момент когда пара создана
+                    final RabbitsNode upperPairAsSelfLink = new RabbitsNode();
+                    upperPairAsSelfLink.setPairAbsoluteCounter(rabbitsNode.pairAbsoluteCounter);
+                    upperPairAsSelfLink.setPairLiveCounter(tempCounter);
+                    rabbitsNode.upperPairAsSelfLink = upperPairAsSelfLink; //ссылка на копию себя
+                    rabbitsNode.lowerPairAsChild = null; // потомок не производится
+                } else
+                if (rabbitsNode.pairLiveCounter > 1) {
+                    final RabbitsNode upperPairAsSelfLink = new RabbitsNode();
+                    upperPairAsSelfLink.setPairAbsoluteCounter(rabbitsNode.pairAbsoluteCounter);
+                    upperPairAsSelfLink.setPairLiveCounter(tempCounter);
+                    rabbitsNode.upperPairAsSelfLink = upperPairAsSelfLink; //ссылка на копию себя
+                    rabbitsNode.lowerPairAsChild = new RabbitsNode(rabbitsNode.pairAbsoluteCounter); // потомок производится
+                } else
+                if (rabbitsNode.pairLiveCounter == 1) {
+                    rabbitsNode.isPairAlive = false; //кролики сдохли
+                    rabbitsNode.upperPairAsSelfLink = null; //кроликов больше нету
+                    rabbitsNode.lowerPairAsChild = new RabbitsNode(rabbitsNode.pairAbsoluteCounter); // потомок производится
+                }
+                rabbitsNode.pairLiveCounter = tempCounter; //уменьшили счетчик жизни
+            }
+        }
+    }
+
+    public static void performLeafNodes(RabbitsNode rabbitsNode, int counter, int MONTHS) {
+        if (counter >= MONTHS) {
+            return;
+        }
+        counter += 1;
+        RabbitsNode upperPairAsSelfLink_month2 = rabbitsNode.getUpperPairAsSelfLink();
+        if (upperPairAsSelfLink_month2 != null) {
+            addHistoricalPairNode(upperPairAsSelfLink_month2);
+            performLeafNodes(upperPairAsSelfLink_month2, counter, MONTHS);
+        }
+        RabbitsNode lowerPairAsChild_month2 = rabbitsNode.getLowerPairAsChild();
+        if (lowerPairAsChild_month2 != null) {
+            addHistoricalPairNode(lowerPairAsChild_month2);
+            performLeafNodes(lowerPairAsChild_month2, counter, MONTHS);
+        }
+    }
+
+    public static int countLeafsAtTreeAtMaximumDepth(RabbitsNode rabbitsNode) {
+        int result = 0;
+        final LinkedList<RabbitsNode> queue = new LinkedList<>();
+        queue.addLast(rabbitsNode);
+        while (!queue.isEmpty()) {
+            RabbitsNode rabbitsNodeFromQueue = queue.removeFirst();
+            if (rabbitsNodeFromQueue.getLowerPairAsChild() == null && rabbitsNodeFromQueue.getUpperPairAsSelfLink() == null) { //the lowest leaf
+                result += 1;
+            }
+            if (rabbitsNodeFromQueue.getLowerPairAsChild() != null) {
+                queue.addLast(rabbitsNodeFromQueue.getLowerPairAsChild());
+            }
+            if (rabbitsNodeFromQueue.getUpperPairAsSelfLink() != null) {
+                queue.addLast(rabbitsNodeFromQueue.getUpperPairAsSelfLink());
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int MONTHS = 9;
+        RabbitsNode rabbitsNode = new RabbitsNode(3);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, MONTHS); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        System.out.println(leafs);
+    }
+}
---
+++ src/test/java/bioinf_stronghold/_9_fibd/RabbitsNodeTest.java
@@ -0,0 +1,147 @@
+package bioinf_stronghold._9_fibd;
+
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static bioinf_stronghold._9_fibd.RabbitsNode.addHistoricalPairNode;
+import static bioinf_stronghold._9_fibd.RabbitsNode.countLeafsAtTreeAtMaximumDepth;
+import static bioinf_stronghold._9_fibd.RabbitsNode.performLeafNodes;
+import static org.junit.Assert.assertEquals;
+
+public class RabbitsNodeTest {
+
+    // month 2, leafs 1
+    // month 3, leafs 2
+    // month 4, leafs 4
+    // month 5, leafs 3
+    // month 6, leafs 4
+    // month 7, leafs 5
+    // month 8, leafs 7
+    // month 9, leafs 9
+
+    @Test
+    public void buildRabbitsThree1() {
+        int N = 1; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(1, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree2() {
+        int N = 2; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(1, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree3() {
+        int N = 3; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(2, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree4() {
+        int N = 4; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(2, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree5() {
+        int N = 5; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(3, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree6() {
+        int N = 6; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(4, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree7() {
+        int N = 7; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(5, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree8() {
+        int N = 8; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(7, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThree9() {
+        int N = 9; // общий срок расчетов
+        int M = 3; //срок жизни одной пары
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        assertEquals(9, leafs);
+    }
+
+    @Test
+    public void buildRabbitsThreeFree() {
+        int N = 6; // общий срок расчетов
+        int M = 4; //срок жизни одной пары
+        LocalDateTime before = LocalDateTime.now();
+        System.out.println("Before " + before + "\n");
+
+        RabbitsNode rabbitsNode = new RabbitsNode(M);  //month 1
+        addHistoricalPairNode(rabbitsNode);//month 2
+        performLeafNodes(rabbitsNode, 2, N); //2 because month is two.
+
+        LocalDateTime middle = LocalDateTime.now();
+        System.out.println("Middle " + middle + "\n");
+        System.out.println("Duration to build tree " + Duration.between(before, middle).toMillis() + " milliseconds" + "\n");
+
+        int leafs = countLeafsAtTreeAtMaximumDepth(rabbitsNode);
+        System.out.println("Leafs: " + leafs + "\n");
+
+        LocalDateTime after = LocalDateTime.now();
+        System.out.println("After " + after + "\n");
+        System.out.println("Duration to count leafs " + Duration.between(middle, after).toMillis() + " milliseconds" + "\n");
+        System.out.println("Duration full " + Duration.between(before, after).toMillis() + " milliseconds" + "\n");
+    }
+
+}
